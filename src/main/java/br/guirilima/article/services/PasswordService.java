5842f891c9a4b3dc66845e18333d99ef6964cdcb
Guirilima <ungui@outlook.com.br>
2022-01-28 21:48:59 -0300

    add more messages

--- src/main/java/br/guirilima/article/services/PasswordService.java
+++ src/main/java/br/guirilima/article/services/PasswordService.java
@@ -3,11 +3,15 @@ package br.guirilima.article.services;
 import br.guirilima.article.models.PasswordRequestDTO;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class PasswordService {
 
     public void saveData(PasswordRequestDTO requestDTO) {
 
         System.out.println("Valido !");
+
     }
 }
